64140f1ab5ea0e91eb98cb8215cfad0c84a59c94
Alexandre Baudry <alexandrebaudry@MacBook-Pro-de-Alexandre.local>
2023-01-13 14:54:46 +0100

    Initial commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function myFunction() {
   }
 
 function myFunctionContact() {
-    if(document.getElementById("contact-hide").style.display = "block"){
+    if(document.getElementById("contact-hide").style.display === "block"){
         document.getElementById("contact-hide").style.display = "none";
     }
     else{
