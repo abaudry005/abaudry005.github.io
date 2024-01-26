9a07da229099c07ad543232792aaf1429f6c4cae
Alexandre Baudry <alexandrebaudry@MacBook-Pro-de-Alexandre.local>
2024-01-26 18:04:16 +0100

    Initial commit

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function updateContent() {
       document.getElementById('linkedin').textContent = data.linkedin;
       document.getElementById('contact').textContent = data.contact;
       document.getElementById('skills').textContent = data.skills;
+      document.getElementById('developer').textContent = data.developer;
     });
 }
 
