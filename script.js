251a7975b86b1d1687fad45f80da4a0c08693547
Alexandre Baudry <alexandrebaudry@MacBook-Pro-de-Alexandre.local>
2024-01-28 02:29:35 +0100

    Initial commit

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function myFunctionContact() {
 
 /*------Game------*/
 
-const cards = ['images/image1.png', 'images/image2.png', 'images/image3.png', 'images/image4.png', 'images/image5.png', 'images/image6.png', 'images/image7.png', 'images/image8.png'];
+const cards = ['images/image1.png', 'images/image2.png', 'images/image3.png', 'images/image4.png', 'images/image5.png', 'images/image6.png', 'images/image7.png', 'images/image8.png,images/image1.png', 'images/image2.png', 'images/image3.png', 'images/image4.png', 'images/image5.png', 'images/image6.png', 'images/image7.png', 'images/image8.png'];
 let flippedCards = [];
 let matchedCards = [];
 
